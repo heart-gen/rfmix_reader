2f7a6f312768d784f92cc79c2da2a976be8169f7
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-05-28 09:59:39 -0400

    adding reading functions for rfmix.Q files

--- rfmix_reader/read_rfmix.py
+++ rfmix_reader/read_rfmix.py
@@ -11,10 +11,13 @@ from collections import OrderedDict as odict
 from xarray import DataArray
 from pandas import DataFrame, read_csv
 
-from .chunk import Chunk
+from chunk import Chunk
 
 __all__ = ["read_rfmix"]
 
+############ Testing ##############
+file_prefix = "/dcs05/lieber/hanlab/jbenjami/projects/localQTL_manuscript/local_ancestry_rfmix/_m"
+###################################
 
 def read_rfmix(file_prefix, verbose=True):
     """
@@ -48,11 +51,64 @@ def read_rfmix(file_prefix, verbose=True):
     from dask.array import concatenate
 
     file_prefixes = sorted(glob(file_prefix))
-    if len(file_prefix) == 0:
+    if len(file_prefixes) == 0:
         file_prefixes = [file_prefix.replace("*", "")]
 
     file_prefixes = sorted(_clean_prefixes(file_prefixes))
+    fn = []
+    for fp in file_prefixes:
+        fn.append({s: f"{fp}.{s}" for s in ["fb.tsv", "rfmix.Q"]})
+        
+    pbar = tqdm(desc="Mapping files", total=2*len(fn), disable=not verbose)
     return None
+
+
+def _read_file(fn, read_func, pbar):
+    data = [];
+    for f in fn:
+        data.append(read_func(f))
+        pbar.update(1)
+    return data
+
+
+def _read_csv(fn, header) -> DataFrame:
+    df = read_csv(
+        fn,
+        delim_whitespace=True,
+        header=None,
+        names=list(header.keys()),
+        dtype=header,
+        comment="#",
+        compression=None,
+        engine="c",
+        iterator=False,
+    )
+    assert isinstance(df, DataFrame)
+    return df
+
+
+def _read_Q(fn):
+    df = _read_Q_noi(fn)
+    df["i"] = range(df.shape[0])
+    return df
+
+
+def _read_Q_noi(fn):
+    header = odict(_types(fn))
+    return _read_csv(fn, header)
+
+
+def _types(fn):
+    from pandas import StringDtype
+    df = read_csv(
+        fn,
+        delim_whitespace=True,
+        nrows=2,
+        skiprows=1,
+    )    
+    header = {"sample_id": StringDtype()}
+    header.update(df.dtypes[1:].to_dict())
+    return header
 
 
 def _clean_prefixes(prefixes):
@@ -60,13 +116,8 @@ def _clean_prefixes(prefixes):
     for p in prefixes:
         dirn = dirname(p)
         basen = basename(p)
-        base = ".".join(basen.split(".")[:-1])
-        if len(base) == 0:
-            path = p
-        else:
-            ext = basen.split(".")[-1]
-            if ext not in ["Q", "tsv"]:
-                base += "." + ext
+        base = basen.split(".")[0]
+        if base != "logs":
             path = join(dirn, base)
-        paths.append(path)
+            paths.append(path)
     return list(set(paths))
