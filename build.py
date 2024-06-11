6499bcc9b0910ae057951cd0514e50789c5c76e6
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-06-11 19:55:26 -0400

    modifying for 64-bit compiler

--- build.py
+++ build.py
@@ -3,8 +3,6 @@ from setuptools import setup
 def build(setup_kwargs):
     setup_kwargs.update(
         setup_requires=['cffi'],
-        cffi_modules=["build_ext.py:ffibuilder"],
-        install_requires=["cffi"],
+        cffi_modules="build_ext.py:ffibuilder",
         zip_safe=False,
-        include_package_data=True,
     )
--- build_ext.py
+++ build_ext.py
@@ -3,6 +3,7 @@ Adapted from `build_ext.py` script in the `pandas-plink` package.
 Source: https://github.com/limix/pandas-plink/blob/main/build_ext.py
 """
 from cffi import FFI
+from sysconfig import get_paths
 from os.path import dirname, abspath, join
 
 def read_lines(file_path):
@@ -10,32 +11,32 @@ def read_lines(file_path):
         content = f.read()
     return content
 
+# Get the current directory
+current_dir = join(dirname(abspath(__file__)))
+# Create an instance of FFI
+ffibuilder = FFI()
+# Set Unicode mode to False
+ffibuilder.set_unicode(False)
 
-def main():
-    # Get the current directory
-    current_dir = join(dirname(abspath(__file__)))
-    # Create an instance of FFI
-    ffibuilder = FFI()
-    # Set Unicode mode to False
-    ffibuilder.set_unicode(False)
+# Configure pyconfig.h
+python_include_path = get_paths()['include']
+pyconfig_64_path = join(python_include_path, 'pyconfig-64.h')
 
-    # Read the header file
-    h_file = join(current_dir, "rfmix_reader", "_fb_reader.h")
-    h_content = read_lines(h_file)
-    # Parse the header file
-    ffibuilder.cdef(h_content)
+# Read the header file
+h_file = join(current_dir, "rfmix_reader", "_fb_reader.h")
+h_content = read_lines(h_file)
 
-    # Read the C source file
-    c_file = join(current_dir, "rfmix_reader", "_fb_reader.c")
-    c_content = read_lines(c_file)
-    # Set the source code and language
-    ffibuilder.set_source("rfmix_reader.fb_reader",
-                          c_content, language="c")
-    return ffibuilder
+# Parse the header file
+ffibuilder.cdef(f"#include {pyconfig_64_path}\n{h_content}")
+
+# Read the C source file
+c_file = join(current_dir, "rfmix_reader", "_fb_reader.c")
+c_content = read_lines(c_file)
+# Set the source code and language
+ffibuilder.set_source("rfmix_reader.fb_reader",
+                      c_content, language="c")
 
 
 if __name__ == "__main__":
-    # Parse files
-    ffibuilder = main()
     # Compile the module
     ffibuilder.compile(verbose=True)
