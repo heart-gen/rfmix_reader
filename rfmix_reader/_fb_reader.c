0d08a42444b8332fb1e4b30edb3f2f58fd32b31b
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-06-19 19:17:03 -0400

    removing unused variable -- for troubleshooting

--- rfmix_reader/_fb_reader.c
+++ rfmix_reader/_fb_reader.c
@@ -17,7 +17,7 @@
 void read_fb_chunk(float *buff, uint64_t nrows, uint64_t ncols,
                    uint64_t row_start, uint64_t col_start, uint64_t row_end,
                    uint64_t col_end, int32_t *out, uint64_t *strides) {
-  uint64_t r, c, ce;
+  uint64_t r, c;
   uint64_t row_size = ncols;
 
   // Start from the row_start
