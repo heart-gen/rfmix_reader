13fb51380465590753dddfa8e8e75689eb670f11
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-06-21 09:24:09 -0400

    load in missing function

--- rfmix_reader/_read_rfmix.py
+++ rfmix_reader/_read_rfmix.py
@@ -12,8 +12,8 @@ from os.path import basename, dirname, join, exists
 
 from ._chunk import Chunk
 from ._fb_read import read_fb
+from ._utils import generate_binary_files
 from ._utils import set_gpu_environment, _process_file
-
 
 try:
     from torch.cuda import is_available
