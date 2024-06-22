b432cb9bbc6cf01b9031e4bf902db0e10c21af42
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-06-22 13:17:16 -0400

    adjustments to improve memory consumption

--- rfmix_reader/_fb_read.py
+++ rfmix_reader/_fb_read.py
@@ -2,8 +2,9 @@
 Adapted from `_bed_read.py` script in the `pandas-plink` package.
 Source: https://github.com/limix/pandas-plink/blob/main/pandas_plink/_bed_read.py
 """
-from numpy import float32, memmap
-from dask.array import from_array, Array
+from dask.delayed import delayed
+from numpy import float32, memmap, array
+from dask.array import from_array, Array, concatenate
 
 __all__ = ["read_fb"]
 
@@ -28,14 +29,34 @@ def read_fb(
     if row_chunk <= 0 or col_chunk <= 0:
         raise ValueError("row_chunk and col_chunk must be positive integers.")
     # Calculate row size and total size for memory mapping
-    try:
-        buff = memmap(filepath, dtype=float32, mode="r", offset=0,
-                      shape=(nrows,ncols))
-        X = from_array(buff, chunks=(row_chunk, col_chunk))
-        buff._mmap.close()
-        del buff
-    except Exception as e:
-        raise IOError(f"Error reading file: {e}")    
+    chunks = []
+    for ii in range(0, nrows, row_chunk):
+        num_rows = min(row_chunk, nrows - ii)
+        delayed_chunk = delayed(_read_chunk, None, True, None, False)(
+            filepath, ii, num_rows, ncols)
+        dask_array_chunk = from_array(delayed_chunk,
+                                      chunks=(num_rows, col_chunk))
+        chunks.append(dask_array_chunk)
+    # Concategnate all chunks
+    X = concatenate(chunks, axis=0, True)
     assert isinstance(X, Array)
     return X
 
+
+def _read_chunk(filepath, start_row, num_rows, num_cols):
+    """
+    Read a chunk of data from the binary file.
+
+    Parameters:
+    filepath (str): Path to the binary file.
+    start_row (int): Starting row index for the chunk.
+    num_rows (int): Number of rows in the chunk.
+    num_cols (int): Number of columns in the chunk.
+
+    Returns:
+    np.ndarray: Chunk of data.
+    """
+    with memmap(filepath, dtype=float32, mode="r",
+                offset=start_row * num_cols * float32().itemsize,
+                shape=(num_rows, num_cols)) as buff:
+        return array(buff)
