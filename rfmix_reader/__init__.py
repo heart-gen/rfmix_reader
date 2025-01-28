91937ba6a87992c08df8848f78400c0365aa3bd8
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-01-28 11:59:11 -0600

    importing the writing functions

--- rfmix_reader/__init__.py
+++ rfmix_reader/__init__.py
@@ -1,6 +1,7 @@
 from ._chunk import Chunk
 from ._fb_read import read_fb
 from ._read_rfmix import read_rfmix
+from ._write_bed import write_bed, write_imputed
 from ._errorhandling import BinaryFileNotFoundError
 from ._imputation import interpolate_array, _expand_array
 from ._loci_bed import (
@@ -13,13 +14,15 @@ from ._utils import (
     get_prefixes, create_binaries
 )
 
-__version__ = "0.1.21"
+__version__ = "0.1.22"
 
 __all__ = [
     "Chunk",
     "read_fb",
+    "write_bed",
     "read_rfmix",
     "__version__",
+    "write_imputed",
     "set_gpu_environment",
     "generate_tagore_bed",
     "BinaryFileNotFoundError",
